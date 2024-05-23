418d0cf190defa7f66b8bf4c8f2320c5971a66f8
kavyagujja236 <gujjakavya286@gmail.com>
2024-05-23 21:04:35 +0530

    first commit

--- salary/models.py
+++ salary/models.py
@@ -3,12 +3,14 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
-    first_name=models.CharField(max_length=100)
-    last_name=models.CharField( max_length=100)
+    name=models.CharField(max_length=100)
+    scientific_name=models.CharField(max_length=100)
+    use=models.CharField(max_length=100)
     photo=models.ImageField(upload_to='images')
-    designation=models.CharField(max_length=100)
-    email_address=models.CharField(max_length=100)
-    phone_number=models.CharField(max_length=10)
-
+    season=models.CharField(max_length=10)
+    cost=models.CharField(max_length=10,blank=True)
     def __str__(self):
-        return self.first_name
+        return self.name
+    
+
+    
